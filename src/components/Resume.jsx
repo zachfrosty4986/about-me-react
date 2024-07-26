afa651753930a7fe65ae51c3e2cc0a0de4669bad
zachfrosty4986 <zachjames0114@gmail.com>
2024-07-25 21:10:26 -0400

    Added syntax to Resume.jsx

---
+++ src/components/Resume.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <section id="resume">
+      <a href="path-to-resume.pdf" download>Download Resume</a>
+      <ul>
+        <li>Proficiency 1</li>
+        <li>Proficiency 2</li>
+        {/* Add more proficiencies */}
+      </ul>
+    </section>
+  );
+};
+
+export default Resume;

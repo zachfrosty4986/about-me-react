cea1d15c4ae114336d39a1cf14b278d0d47b0c52
zachfrosty4986 <zachjames0114@gmail.com>
2024-07-25 21:08:00 -0400

    Added starter code to AboutMe.jsx

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section id="about-me">
+      <img src="path-to-avatar.jpg" alt="Developer Avatar" />
+      <p>Short bio about the developer...</p>
+    </section>
+  );
+};
+
+export default AboutMe;

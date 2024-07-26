9aa148d3547245488ee0189fc81762f1b5ed7229
zachfrosty4986 <zachjames0114@gmail.com>
2024-07-25 21:07:20 -0400

    Added syntax to Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://github.com/username" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="https://linkedin.com/in/username" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a href="https://twitter.com/username" target="_blank" rel="noopener noreferrer">Twitter</a>
+    </footer>
+  );
+};
+
+export default Footer;

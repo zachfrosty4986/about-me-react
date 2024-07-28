2a7b8955909891b2183d234287a1c89f6a38825b
zachfrosty4986 <zachjames0114@gmail.com>
2024-07-28 19:48:43 -0400

    Debugged code

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import AboutMe from './components/AboutMe';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
+import './App.css'
+// import AboutMe from './components/AboutMe';
+// import Portfolio from './components/Portfolio';
+// import Contact from './components/Contact';
+// import Resume from './components/Resume';
 
 const App = () => {
   return (
-    <Router>
       <div>
         <Header />
         <main>
-          <Switch>
-            <Route exact path="/" component={AboutMe} />
-            <Route path="/portfolio" component={Portfolio} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/resume" component={Resume} />
-          </Switch>
+          < Outlet />
         </main>
         <Footer />
       </div>
-    </Router>
   );
 };
 
---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx'
+import './index.css'
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />,
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+      {
+        path: '/resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)

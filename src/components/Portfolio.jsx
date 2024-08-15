29efe33365b71bea752a8d0133f82b303dbb3b01
zachfrosty4986 <zachjames0114@gmail.com>
2024-08-15 16:27:05 -0400

    Added project links to profile

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Portfolio = () => {
   const projects = [
-    { title: 'Project 1', imgSrc: 'path-to-image1.jpg', deployUrl: 'http://example.com', repoUrl: 'http://github.com/example' },
-    // Add more projects here
+    { title: 'My Work Portfolio', imgSrc: 'src\assets\portfolio.png', deployUrl: 'https://zachfrosty4986.github.io/My-Work-Porfolio/', repoUrl: 'https://github.com/zachfrosty4986/My-Work-Porfolio' },
+    { title: 'My Task Board', imgSrc: 'src\assets\tasks.png', deployUrl: 'https://zachfrosty4986.github.io/My-Task-Board/', repoUrl: 'https://github.com/zachfrosty4986/My-Task-Board' },
+    { title: 'Brewsters Brewery finder', imgSrc: 'src\assets\brewsters.png', deployUrl: 'https://zachfrosty4986.github.io/interactive-brewery-map/', repoUrl: 'https://github.com/zachfrosty4986/interactive-brewery-map' },
+    { title: 'Prework Study Guide', imgSrc: 'src\assets\prework.png', deployUrl: 'https://zachfrosty4986.github.io/prework-study-guide/', repoUrl: 'https://github.com/zachfrosty4986/prework-study-guide' },
+    { title: 'Personal Blog', imgSrc: 'src\assets\blog.png', deployUrl: 'https://zachfrosty4986.github.io/My-Personal-Blog/', repoUrl: 'https://github.com/zachfrosty4986/My-Personal-Blog' },
   ];
 
   return (
